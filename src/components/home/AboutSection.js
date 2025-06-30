1de6fb0e4488b0e79136c1bc6ee3042fe2401d7b
patrik <patrik.biblak@gmail.com>
2025-07-01 00:46:04 +0200

    test

--- src/components/home/AboutSection.js
+++ src/components/home/AboutSection.js
@@ -16,7 +16,7 @@ const AboutSection = () => {
           <h2>O mne</h2>
           <p>Venujem sa front-end vývoju a neustále sa učím nové technológie, aby som mohol prinášať funkčné a vizuálne príťažlivé riešenia.</p>
           <p>Mám skúsenosti s HTML, CSS, JavaScriptom a pracujem najmä s Reactom pri tvorbe dynamických používateľských rozhraní.</p>
-          <p>Rád budujem veci od nuly a spolupracujem s ľuďmi, ktorí chcú kvalitný a spoľahlivý web – či už ide o portfólio, blog, e-shop alebo firemnú stránku.</p>
+          <p>Rád budujem veci od nuly a spolupracujem s ľuďmi, ktorí chcú kvalitný a spoľahlivý web či už ide o portfólio, blog, e-shop alebo firemnú stránku.</p>
         </div>
         <div className="about-item" ref={rightRef}>
           <img src="images/profile.png" alt="profile" />
